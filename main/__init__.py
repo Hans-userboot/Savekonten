3ce186cfa9f4fcbf20fa828375be51a8a8e24212
Hans-userboot <100776056+Hans-userboot@users.noreply.github.com>
2023-01-31 05:20:28 +0700

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,14 +12,14 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
-AUTH = config("AUTH", default=None, cast=int)
+API_ID = config("22783302", default=None, cast=int)
+API_HASH = config("d2d9ed1561e4e8c8ab2d99d10742ba3d", default=None)
+BOT_TOKEN = config("6194231915:AAHrfdvFtZPZFr6prYctAtkD6t1wEbySpKw", default=None)
+SESSION = config("BQBapoFKC0yDITGFc1WDI_MCPJfiENi868uQQV92Uce3Chv7MM2vLmUPQ0Ysb4U33COQuuX-RnF1Fe2rhYGMEBOfN5zdxP9nxTlsHW7rQxsuQDFidWqS7WCk0kuAeZO9iFUvqpAcp1oU3EytBEdS8binLjiceYtrCj_Lrf8XmY-i1OhxbC_CvP71wt1EyNydoyz4yQH7_iGuGHQj3AzLRmhwZFz6OS2QFVZ1QrbeMAaefW_vhw-HTeIAPD2mlC-_dEO6z8nwUVcEQUBYwcqnNlF05FvGPZMuII9RERCMmNJKUErOoBFk3ZMYoirKRQ96WVUqm0nRktQjKI9RVOwI74Zlc-G4OAA", default=None)
+FORCESUB = config("-1001613168852", default=None)
+AUTH = config("1723017752", default=None, cast=int)
 
-bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
+bot = TelegramClient(bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
 userbot = Client(
     session_name=SESSION, 
