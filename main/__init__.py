58652d449f56b6fe1b77e1ead67a6c81496fd030
Hans-userboot <100776056+Hans-userboot@users.noreply.github.com>
2023-01-31 05:24:52 +0700

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -17,7 +17,7 @@ API_HASH = config("d2d9ed1561e4e8c8ab2d99d10742ba3d", default=None)
 BOT_TOKEN = config("6194231915:AAHrfdvFtZPZFr6prYctAtkD6t1wEbySpKw", default=None)
 SESSION = config("BQBapoFKC0yDITGFc1WDI_MCPJfiENi868uQQV92Uce3Chv7MM2vLmUPQ0Ysb4U33COQuuX-RnF1Fe2rhYGMEBOfN5zdxP9nxTlsHW7rQxsuQDFidWqS7WCk0kuAeZO9iFUvqpAcp1oU3EytBEdS8binLjiceYtrCj_Lrf8XmY-i1OhxbC_CvP71wt1EyNydoyz4yQH7_iGuGHQj3AzLRmhwZFz6OS2QFVZ1QrbeMAaefW_vhw-HTeIAPD2mlC-_dEO6z8nwUVcEQUBYwcqnNlF05FvGPZMuII9RERCMmNJKUErOoBFk3ZMYoirKRQ96WVUqm0nRktQjKI9RVOwI74Zlc-G4OAA", default=None)
 FORCESUB = config("-1001613168852", default=None)
-AUTH = config("1723017752", default=None, cast=int)
+AUTH = config("1944172600", default=None, cast=int)
 
 bot = TelegramClient(bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
